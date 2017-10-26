71b6d83030d88308ac125bd68613a5ea3bb99e4a
hurley <sean.hurley6@gmail.com>
2017-10-26 08:42:09 -0500

    Day 23 part 2

--- lib/adventofcode2016.ex
+++ lib/adventofcode2016.ex
@@ -42,6 +42,7 @@ defmodule AdventOfCode2016 do
       "21.2" -> Day21.unscramble_from_file("fbgdceah", "data/day21.txt")
       "22" -> Day22.count_viable_pairs_from_file("data/day22.txt")
       "23" -> Day12.evaluate_from_file("data/day23.txt")
+      "23.2" -> Day12.evaluate_from_file("data/day23.2.txt")
     end
     IO.inspect result, limit: :infinity
   end
